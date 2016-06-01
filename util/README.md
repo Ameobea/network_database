df7b4b51cd86c1a6ea8189a1c9001806ee281de5
Casey Primozic <me@ameo.link>
2016-06-01 17:11:45 -0500

    Created python dump to mongodb import script

---
+++ util/README.md
@@ -0,0 +1,2 @@
+# Utility Functions
+This directory contains scripts used to process data for use in the database.  They are not a direct component of the web application, but may be used by the application directly or indirectly as a way to convert data from other formats, perform regular maintenance operations, or other similar tasks.
---
+++ util/jsonToMongo.py
@@ -0,0 +1,56 @@
+# JSON dump data importer
+#
+# Casey Primozic
+#
+# This script imports data from JSON dumps taken from various parts
+# of the processing pipeline and imports them into MongoDB.
+#
+# Usage:
+# python jsonToMongo.py -c correlation_results.json -d postAnalysisResults.json
+
+import json, sys, argparse
+from pymongo import MongoClient
+from os import path
+
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+
+from helpers import conf
+
+client = MongoClient(conf.MONGO_ADDRESS, conf.MONGO_PORT)
+db = client[conf.MONGO_DATABASE]
+
+mainResultsFilename = False
+
+parser = argparse.ArgumentParser(description='Input filename.')
+parser.add_argument("-d")
+args = parser.parse_args()
+globals().update(vars(args))
+if d:
+  mainResultsFilename = d
+
+def printUsage():
+  print("Usage: python jsonToMongo.py -d postAnalysisResults.json")
+  sys.exit(2)
+
+def importMain(filename):
+  inFile = open(filename, "r")
+  inData = json.load(inFile)
+  for networkName, networkData in inData.iteritems():
+    networkHash = networkData[0]["hash"]
+    cursor = db.networks.find({"hash": networkHash})
+    if cursor.count() == 0:
+      result = db.networks.insert_one({"hash": networkHash})
+      print(result)
+    else:
+      doc = cursor[0]
+      for calculation in networkData:
+        if not(calculation["name"] in doc):
+          doc[calculation["name"]] = calculation["data"]
+      db.networks.update_one({"hash": networkHash}, {"$set": doc})
+
+
+if type(mainResultsFilename) != bool:
+  importMain(mainResultsFilename)
+else:
+  print("No input file was supplied.")
+  printUsage()
