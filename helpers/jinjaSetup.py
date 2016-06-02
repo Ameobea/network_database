65e7e713c21a312d97a7b3657cb74ce5f11a6e2f
Casey Primozic <me@ameo.link>
2016-06-02 15:31:32 -0500

    Added all of the network stats to the `info` page

--- helpers/jinjaSetup.py
+++ helpers/jinjaSetup.py
@@ -5,17 +5,53 @@
 import re
 from jinja2 import evalcontextfilter, Markup, escape
 
+# Makes sure string input is a number 1-50 and returns int form; if not returns 10
 def countFilter(count):
   try:
     count = int(count)
-    if count <= 50:
+    if count <= 50 or count <= 0:
       return count
     else:
       return 10
   except Exception, e:
     return 10
 
+# Converts list of calculations into dictionary
+def calcDictFilter(calcList):
+  res = {}
+  for calc in calcList:
+    res[calc["name"]] = calc["data"]
+  return res
+
+# Adds commas in to a number
+def commaFilter(inNum):
+  if type(inNum) == int or type(inNum) == float:
+    return "{:,}".format(inNum)
+  else:
+    return inNum
+
+def calcExists(inCalc):
+  try:
+    res = inCalc["res"]
+    if type(res) == int or type(res) == float or type(res) == dict:
+      return True
+    elif type(res) == dict:
+      pass # TODO
+    else:
+      if type(res) == bool or res == "False" or res == "True":
+        return True
+      else:
+        print inCalc["res"]
+        return False
+  except Exception, e:
+    return False
+
+# Installs the filters in the global Jinja environment
 def register(environment):
   environment.line_statement_prefix = '%'
 
   environment.filters["count"] = countFilter
+  environment.filters["calcDict"] = calcDictFilter
+  environment.filters["commas"] = commaFilter
+
+  environment.tests["validCalc"] = calcExists
