e680a8c63ac759540bb18025a6416e2ffbfbabe4
Casey Primozic <me@ameo.link>
2016-06-08 00:45:32 -0500

    Added comparison add button to info page Moved comparison add cookie management function into separate file

---
+++ sources/compareAdd.js
@@ -0,0 +1,26 @@
+function compareAdd(hash){ // Add it to cookie
+  var cookieValue = Cookies.get("compare");
+  if(cookieValue){
+    var splitCookie = cookieValue.split(",");
+  }
+
+  if(cookieValue && splitCookie.indexOf(hash) == -1){
+    cookieValue += "," + hash;
+    Cookies.set("compare", cookieValue);
+    return("-");
+  }else if(!cookieValue){ // Create cookie
+    Cookies.set("compare", hash)
+    return("-");
+  }else{ // Remove it
+    var newCookieValue = "";
+    for(var i=0;i<splitCookie.length;i++){
+      if(splitCookie[i] != hash){
+        newCookieValue += splitCookie[i] + ",";
+      }
+    }
+    
+    newCookieValue = newCookieValue.substr(newCookieValue, newCookieValue.length-1);
+    Cookies.set("compare", newCookieValue);
+    return("+");
+  }
+}
