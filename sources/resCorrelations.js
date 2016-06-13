8b981816e1f4a72b2b8faa4c34ed1771a8fc94d7
Casey Primozic <me@ameo.link>
2016-06-12 19:17:27 -0500

    Got basic scatter plot functionality working

--- sources/resCorrelations.js
+++ sources/resCorrelations.js
@@ -1,13 +1,103 @@
 "use strict";
-/*jslint browser: true*/ /*global  $*/
+/*jslint browser: true*/ /*global  $, console, suboptMaps, networks*/
+
+// Generates the HTML for the option tags that go inside the suboption <select> tag
+function suboptGen(suboptName, suboptMap, num){
+  var res = "<select id=\"suboptSel" + num + "\">\n";
+  for(var i=0;i<suboptMap.length;i++){
+    if(!(suboptMap.length > 1 && suboptMap[i] == "res")){ // Ignore res if we have processed calculations instead
+      res += "<option value=\"" + suboptMap[i] + "\">" + suboptMap[i] + "</option>\n";
+    }
+  }
+  res += "</select>";
+  return res;
+}
+
+// Generates a list of correlation data to be plotted for each network
+// that contains valid data, inserts into chart series, and redraws chart
+function genCorrelations(corr1, corr2, subcorr1, subcorr2){
+  console.log(corr1, corr2, subcorr1, subcorr2);
+  var res = [];
+  for(var i=0;i<networks.length;i++){
+    if(networks[i].calculations[corr1].data[subcorr1] &&
+        networks[i].calculations[corr2].data[subcorr2]){ // both subcalcs exist
+      res.push([
+        networks[i].calculations[corr1].data[subcorr1],
+        networks[i].calculations[corr2].data[subcorr2]
+      ]);
+    }
+  }
+
+  var scatterplot = $("#scatterplot").highcharts();
+  scatterplot.series[0].remove(true);
+  scatterplot.addSeries({data: res, name: corr1 + "-" +
+      subcorr1 + " " + corr2 + "-" + subcorr2});
+  scatterplot.xAxis.title = corr1 + "-" + subcorr1
+  scatterplot.yAxis.title = corr2 + "-" + subcorr2
+  scatterplot.redraw();
+}
+
 $(document).ready(function(){
+  // Create blank scatter plot
   $("#scatterplot").highcharts({
     chart: {
       type: 'scatter',
       zoomType: 'xy'
     },
+    xAxis: {
+      title: {
+        enabled: true,
+        text: 'test'
+      },
+      startOnTick: true,
+      endOnTick: true,
+      showLastLabel: true
+    },
+    yAxis: {
+      title: {
+        text: 'test'
+      }
+    },
     title: {
       text: "Network Attribute Correlation Plot"
+    },
+    plotOptions: {
+      scatter: {
+        marker: {
+          radius: 5,
+          states: {
+            hover: {
+              enabled: true,
+              lineColor: 'rgb(100,100,100)'
+            }
+          }
+        },
+        states: {
+          hover: {
+            marker: {
+              enabled: false
+            }
+          }
+        }
+      }
+    },
+    series: [{
+      name: "Correlations",
+      data: [[null, null]]
+    }]
+  });
+
+  // Set up attribute selector change listeners
+  $(".attrSelector").change(function(){
+    if(suboptMaps[this.value] != [] && suboptMaps[this.value] != ["res"]){
+      var num = this.id.split("tor")[1];
+      $("#subOption" + num).html(suboptGen(this.value, suboptMaps[this.value], num));
     }
+  });
+
+  // Draw button click listener
+  $("#drawButton").click(function(){
+    genCorrelations($("#attrSelector1").val(), $("#attrSelector2").val(),
+        $("#suboptSel1").val(), $("#suboptSel2").val());
   });
 });
--- views/resCorrelations.py
+++ views/resCorrelations.py
@@ -8,6 +8,20 @@ sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
 from helpers import dbQuery, conf
 
+# generates the suboptions for each calculation from the
+# subcalculations like average, max, min, etc.
+# Creates JSON-encoded object to be passed into JavaScript world
+def genSubopts(networks):
+  res = {}
+  for network in networks:
+    calcs = network["calculations"]
+    for calcName, calcValue in calcs.iteritems():
+      res[calcName] = []
+      for subcalcName, subcalcValue in calcValue["data"].iteritems():
+        if subcalcName != "error" and not(calcName in res[calcName]):
+          res[calcName].append(subcalcName)
+  return json.dumps(res)
+
 # Generates HTML that goes inside of the select dropdown menus for picking which
 # attributes to display in the correlation plot.
 def genAttrOptions(networks):
@@ -21,11 +35,22 @@ def genAttrOptions(networks):
     res += "  <option value=\"" + attr + "\">" + attr + "</option>\n"
   return res
 
+# Removes non-numerical data from network calculations and prepares them for JSON-serialization
+def stripNetworks(networks):
+  for i in range(0, len(networks)):
+    networks[i]["_id"] = None
+    for calcName, calcValue in networks[i]["calculations"].iteritems():
+      for subcalcName, subcalcValue in calcValue["data"].iteritems():
+        if type(subcalcValue) == dict or type(subcalcValue) == list:
+          networks[i]["calculations"][calcName]["data"][subcalcName] = None
+  return json.dumps(networks).replace("'", " ")
+
 def render(b64=""):
   nList = json.loads(base64.b64decode(b64))
   networks = []
   for nHash in nList:
     networks.append(dbQuery.getNetwork(nHash))
-
+  attrOptions = genAttrOptions(networks)
   return render_template("resCorrelations.html", conf=conf, networks=networks,
-      attrOptions=genAttrOptions(networks))
+      attrOptions=attrOptions, suboptionDeclarations=genSubopts(networks),
+      strippedNetworks=stripNetworks(networks))
